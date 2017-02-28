16ac029439940d2f8783d5d979193e03ea138c66
jdpxiaomng <302940116@qq.com>
2017-02-28 17:11:10 +0800

    修复评课详情显示错误问题　　原因:需求发生变更 Memory test: no

--- app/src/main/java/com/codyy/erpsportal/commons/models/entities/EmumIndex.java
+++ app/src/main/java/com/codyy/erpsportal/commons/models/entities/EmumIndex.java
@@ -11,7 +11,7 @@ package com.codyy.erpsportal.commons.models.entities;
 public class EmumIndex {
 
 
-    private static String[] indexs = {"一","二","三","四","五","六","七","八","九"};
+    private static String[] indexs = {"一","二","三","四","五","六","七","八","九","十","十一","十二","十三","十四","十五","十六"};
 
 
     /**
