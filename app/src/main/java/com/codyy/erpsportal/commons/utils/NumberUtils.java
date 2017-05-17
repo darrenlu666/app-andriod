28e5e57cb59bd4a7ae593c5636922a3bc10db8cc
顾佳佳 <gujiajia@codyy.com>
2017-05-17 08:32:03 +0800

    11791 AndroidV5.3.3补充版本-名校网络课堂-实时轮巡 节次 栏 字符错误 模块：巡视

--- app/src/main/java/com/codyy/erpsportal/commons/utils/NumberUtils.java
+++ app/src/main/java/com/codyy/erpsportal/commons/utils/NumberUtils.java
@@ -1,18 +1,35 @@
 package com.codyy.erpsportal.commons.utils;
 
 /**
+ * 数字工具
  * Created by gujiajia on 2016/9/23.
  */
 public class NumberUtils {
 
+    /**
+     * 字符串转为float
+     * @param str 字符串
+     * @return 转换好的float
+     */
     public static float floatOf(String str) {
         float f;
         try {
             f = Float.valueOf(str);
         } catch (NumberFormatException e) {
             e.printStackTrace();
-            f = 0;
+            f = 0f;
         }
         return f;
+    }
+
+    public static int intOf(String str) {
+        int i;
+        try {
+            i = Integer.valueOf(str);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            i = 0;
+        }
+        return i;
     }
 }
