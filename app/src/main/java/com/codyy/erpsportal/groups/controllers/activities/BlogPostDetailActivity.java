2d60013a3039b8445a44cfd36638e8f347c3efef
jdpxiaoming <caixingming@codyy.com>
2017-10-23 17:52:32 +0800

    应用博文详情-推送功能UI调整. v5.3.8准备提测 Memory Test : No

--- app/src/main/java/com/codyy/erpsportal/groups/controllers/activities/BlogPostDetailActivity.java
+++ app/src/main/java/com/codyy/erpsportal/groups/controllers/activities/BlogPostDetailActivity.java
@@ -346,6 +346,7 @@ public class BlogPostDetailActivity extends BaseHttpActivity implements BlogComp
     }
 
     private TextView mPushDoorTv,mPushSlideTv,mPushUpperTv;
+    private View mPushDivider;
     /**
      * 推送选择Menu.
      */
@@ -356,6 +357,8 @@ public class BlogPostDetailActivity extends BaseHttpActivity implements BlogComp
             mPushDoorTv = (TextView) contentView.findViewById(R.id.tv_door);
             mPushSlideTv = (TextView) contentView.findViewById(R.id.tv_slide);
             mPushUpperTv = (TextView) contentView.findViewById(R.id.tv_upper);
+            mPushDivider = contentView.findViewById(R.id.divider3);
+
             TextView exitTv = (TextView) contentView.findViewById(R.id.tv_cancel);
             mPushDoorTv.setOnClickListener(mDialogListener);
             mPushSlideTv.setOnClickListener(mDialogListener);
@@ -393,9 +396,11 @@ public class BlogPostDetailActivity extends BaseHttpActivity implements BlogComp
         switch (mUserInfo.getUserType()){
             case UserInfo.USER_TYPE_AREA_USER:
                 mPushUpperTv.setVisibility(View.GONE);
+                mPushDivider.setVisibility(View.GONE);
                 break;
             case UserInfo.USER_TYPE_SCHOOL_USER://学校
                 mPushUpperTv.setVisibility(View.VISIBLE);
+                mPushDivider.setVisibility(View.VISIBLE);
                 break;
         }
 
